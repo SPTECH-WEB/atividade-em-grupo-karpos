69af7ccae0b0cd0f0c01cec543b820b1f8a0da20
Mauricio <mauricio.almeida@sptech.school>
2025-04-16 18:51:13 -0300

    Configurando o projeto inicial

---
+++ Pedidos/Pedidos/src/test/java/com/atividade/karpos/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade.karpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
